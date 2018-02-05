291ee99c29ebae05e62ca0405135c029c363a798
mojaray2k <amen.ra@imaginationeverywhere.info>
2018-02-05 10:06:37 -0500

    adding reducers

--- reducers.js
+++ reducers.js
@@ -1,5 +1,34 @@
 // Users
-function users (state, action) {
+const initialUserState = {
+  lastUpdated: 0,
+  info : {
+    name: '',
+    uid: '',
+    avatar: ''
+  }
+}
+
+function user (state = initialUserState, action){
+  switch(action.type) {
+    case FETCHING_USER_SUCCESS :
+      return {
+        ...state,
+        info: action.user,
+        lastUpdated: action.timestamp
+      }
+    default:
+      return state
+  }
+}
+
+const initialUsersState = {
+  isFetching: false,
+  error: '',
+  isAuthed: false,
+  authedId: ''
+}
+
+function users (state = initialUsersState, action) {
   switch (action.type) {
     case AUTH_USER :
     /**
@@ -15,5 +44,37 @@ function users (state, action) {
        isAuthd: true,
        authedId: action.uid
      }
+     case UNAUTH_USER :
+      return {
+        ...state,
+        isAuthd: false,
+        authedId: ''
+      }
+    case FETCHING_USER :
+      return {
+        ...state,
+        isFetching: true
+      }
+    case FETCHING_USER_FAILURE :
+      return {
+        ...state,
+        isFetching: false,
+        error: action.error
+      }
+    case FETCHING_USER_SUCCESS :
+      return action.user === null
+        ? {
+          ...state,
+          error: '',
+          isFetching: false
+        }
+        : {
+          ...state,
+          isFetching: false,
+          error: '',
+          [action.uid]: user(state[action.uid], action)
+        }
+      default :
+        return state
   }
 }
